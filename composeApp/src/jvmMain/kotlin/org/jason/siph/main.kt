11167c95d572e7dbe75a70d5a9253884d1a181f1
jason <changhexuefei2011@163.com>
2026-07-18 23:09:09 +0800

    Select hardware runtime mode from JVM property

--- composeApp/src/jvmMain/kotlin/org/jason/siph/main.kt
+++ composeApp/src/jvmMain/kotlin/org/jason/siph/main.kt
@@ -2,12 +2,17 @@ package org.jason.siph
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jason.siph.domain.runtime.HardwareRuntimeMode
 
 fun main() = application {
+    val runtimeMode = HardwareRuntimeMode.parse(
+        System.getProperty("siph.hardware.mode")
+    )
+
     Window(
         onCloseRequest = ::exitApplication,
-        title = "SiphStudio",
+        title = "SiPh Studio · ${runtimeMode.text}",
     ) {
-        App()
+        App(runtimeMode = runtimeMode)
     }
 }
