f8fb2c312c83abfdc3ee0a41d53c7a31161a6459
jason <changhexuefei2011@163.com>
2026-07-18 23:40:18 +0800

    Set mission control desktop window

--- composeApp/src/jvmMain/kotlin/org/jason/siph/main.kt
+++ composeApp/src/jvmMain/kotlin/org/jason/siph/main.kt
@@ -1,17 +1,24 @@
 package org.jason.siph
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import org.jason.siph.domain.runtime.HardwareRuntimeMode
 
 fun main() = application {
     val runtimeMode = HardwareRuntimeMode.parse(
         System.getProperty("siph.hardware.mode")
     )
+    val windowState = rememberWindowState(
+        width = 1500.dp,
+        height = 920.dp
+    )
 
     Window(
         onCloseRequest = ::exitApplication,
-        title = "SiPh Studio · ${runtimeMode.text}",
+        state = windowState,
+        title = "SiPh Studio // Mission Control · ${runtimeMode.text.uppercase()}"
     ) {
         App(runtimeMode = runtimeMode)
     }
