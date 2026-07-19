7b947ed40608ea9066054b22116ba1bd9170a46d
jason <changhexuefei2011@163.com>
2026-07-19 18:43:29 +0800

    Provide in-memory distributed production coordinator on Wasm

--- composeApp/src/wasmJsMain/kotlin/org/jason/siph/di/ProductionPlatform.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/org/jason/siph/di/ProductionPlatform.wasmJs.kt
@@ -1,10 +1,23 @@
 package org.jason.siph.di
 
 import org.jason.siph.domain.production.AuditHasher
+import org.jason.siph.domain.production.DistributedProductionCoordinator
+import org.jason.siph.domain.production.InMemoryDistributedProductionCoordinator
 import org.jason.siph.domain.production.InMemoryProductionRepository
 import org.jason.siph.domain.production.PortableAuditHasher
 import org.jason.siph.domain.production.ProductionRepository
+import org.jason.siph.domain.production.UnavailableDistributedProductionCoordinator
+import org.jason.siph.domain.runtime.HardwareRuntimeMode
 
 actual fun createPlatformProductionRepository(): ProductionRepository = InMemoryProductionRepository()
 
 actual fun createPlatformAuditHasher(): AuditHasher = PortableAuditHasher()
+
+actual fun createPlatformDistributedProductionCoordinator(
+    runtimeMode: HardwareRuntimeMode
+): DistributedProductionCoordinator = when (runtimeMode) {
+    HardwareRuntimeMode.Demo -> InMemoryDistributedProductionCoordinator()
+    HardwareRuntimeMode.Real -> UnavailableDistributedProductionCoordinator(
+        "PostgreSQL distributed production coordination is only available on the JVM target"
+    )
+}
