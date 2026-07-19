8a1498c8d7228458b7df1bff547e4072259a26cc
jason <changhexuefei2011@163.com>
2026-07-19 18:42:51 +0800

    Expose platform distributed production coordinator

--- composeApp/src/commonMain/kotlin/org/jason/siph/di/ProductionPlatform.kt
+++ composeApp/src/commonMain/kotlin/org/jason/siph/di/ProductionPlatform.kt
@@ -1,8 +1,14 @@
 package org.jason.siph.di
 
 import org.jason.siph.domain.production.AuditHasher
+import org.jason.siph.domain.production.DistributedProductionCoordinator
 import org.jason.siph.domain.production.ProductionRepository
+import org.jason.siph.domain.runtime.HardwareRuntimeMode
 
 expect fun createPlatformProductionRepository(): ProductionRepository
 
 expect fun createPlatformAuditHasher(): AuditHasher
+
+expect fun createPlatformDistributedProductionCoordinator(
+    runtimeMode: HardwareRuntimeMode
+): DistributedProductionCoordinator
