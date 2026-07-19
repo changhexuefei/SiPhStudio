c5d586ff5a83d49a73cd4198228d40de530ff3c1
jason <changhexuefei2011@163.com>
2026-07-19 12:09:34 +0800

    Create real PI hardware at JVM startup

--- composeApp/src/jvmMain/kotlin/org/jason/siph/main.kt
+++ composeApp/src/jvmMain/kotlin/org/jason/siph/main.kt
@@ -5,21 +5,29 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import org.jason.siph.domain.runtime.HardwareRuntimeMode
+import org.jason.siph.hardware.pi.createJvmRealHardwarePorts
 
-fun main() = application {
+fun main() {
     val runtimeMode = HardwareRuntimeMode.parse(
         System.getProperty("siph.hardware.mode")
     )
-    val windowState = rememberWindowState(
-        width = 1500.dp,
-        height = 920.dp
-    )
+    val realHardwarePorts = createJvmRealHardwarePorts(runtimeMode)
 
-    Window(
-        onCloseRequest = ::exitApplication,
-        state = windowState,
-        title = "SiPh Studio // Mission Control · ${runtimeMode.text.uppercase()}"
-    ) {
-        App(runtimeMode = runtimeMode)
+    application {
+        val windowState = rememberWindowState(
+            width = 1500.dp,
+            height = 920.dp
+        )
+
+        Window(
+            onCloseRequest = ::exitApplication,
+            state = windowState,
+            title = "SiPh Studio // Mission Control · ${runtimeMode.text.uppercase()}"
+        ) {
+            App(
+                runtimeMode = runtimeMode,
+                realHardwarePorts = realHardwarePorts
+            )
+        }
     }
 }
